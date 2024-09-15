35e47048a2d557e6f5a24c982cc28501bacdda58
Mr. Ryt <125740894+MrRyt247@users.noreply.github.com>
2024-09-15 16:18:11 +0000

    🔧 updates oh-my-posh config

--- config/ohmyposh/Microsoft.PowerShell_profile.ps1
+++ config/ohmyposh/Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,10 @@
-oh-my-posh init pwsh --config 'C:\Users\flaviO\AppData\Local\Programs\oh-my-posh\themes\clean-detailed.omp.json' | Invoke-Expression
+# oh-my-posh init pwsh --config 'C:\Users\{user}\AppData\Local\Programs\oh-my-posh\themes\clean-detailed.omp.json' | Invoke-Expression
+
+oh-my-posh init pwsh --config "C:\Users\{user}\AppData\Local\Programs\oh-my-posh\themes\mrryt.omp.json" | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
 if ($host.Name -eq 'ConsoleHost')
 {
     Import-Module PSReadLine
-}
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
 }
